306adceb1bd6609bebff32280f8cb89d9a1e8f55
Noah Goldberg <noahgold511@gmail.com>
2020-01-30 11:34:33 -0500

    Initial Commit

---
+++ Color Mix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Mix
+//
+//  Created by Noah Goldberg on 1/30/20.
+//  Copyright © 2020 Noah Goldberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

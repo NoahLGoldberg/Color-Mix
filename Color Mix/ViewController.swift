43df47cfc4cc7fe8e2658fc0db2575d9e3fc9a0e
Noah Goldberg <noahgold511@gmail.com>
2020-01-31 11:57:42 -0500

    Color on sceen

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let color = UIColor(displayP3Red: 1.0, green: 1.0, blue: 0.0, alpha: 1)
+        colorView.backgroundColor = color
     }
 
 

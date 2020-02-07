72888f46929fa08086cda95d5d366fcd941b5b8a
Noah Goldberg <noahgold511@gmail.com>
2020-02-07 11:45:47 -0500

    Switches RGB

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -11,14 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
-        colorView.backgroundColor = color
+        colorView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     }
     
     @IBAction func switchChanged(_ sender: UISwitch) {
-        let color = UIColor(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 1)
+        setColor()
+    }
+    
+    func setColor() {
+        let red: CGFloat = redSwitch.isOn ? 1 : 0
+        let green: CGFloat = greenSwitch.isOn ? 1 : 0
+        let blue: CGFloat = blueSwitch.isOn ? 1 : 0
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
         colorView.backgroundColor = color
     }
     

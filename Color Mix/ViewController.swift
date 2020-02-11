87c11d365dce1835035a50f43a7f7d5f7e770aee
Noah Goldberg <noahgold511@gmail.com>
2020-02-10 20:12:02 -0500

    Percentages Added

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
@@ -17,10 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redNumber: UILabel!
+    @IBOutlet weak var greenNumber: UILabel!
+    @IBOutlet weak var blueNumber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        setColor()
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
@@ -31,10 +33,10 @@ class ViewController: UIViewController {
     }
     
     func setColor() {
-        var red = CGFloat(1)
-        var green = CGFloat(1)
-        var blue = CGFloat(1)
-        colorView.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
+        var red:   CGFloat = 1
+        var green: CGFloat = 1
+        var blue:  CGFloat = 1
+        
         if redSwitch.isOn {
             red = CGFloat(redSlider.value)
         }
@@ -45,9 +47,22 @@ class ViewController: UIViewController {
            blue = CGFloat(blueSlider.value)
         }
         
-        let color = UIColor(displayP3Red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1)
+        let color = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1
+        )
         
-        colorView.backgroundColor = color
+        self.view.backgroundColor = color
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        redNumber.text   = String(Int(redSlider.value * 100) ) + "%"
+        greenNumber.text = String(Int(greenSlider.value * 100) ) + "%"
+        blueNumber.text  = String(Int(blueSlider.value * 100) ) + "%"
     }
 }
 
